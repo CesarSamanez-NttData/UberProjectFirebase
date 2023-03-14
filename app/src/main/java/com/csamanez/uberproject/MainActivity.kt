43766c4938700a397d04b8b38a99a23f981a3643
Cesar Samanez Jimenez <csamanez@emeal.nttdata.com>
2023-03-13 19:10:04 -0500

    Design

--- app/src/main/java/com/csamanez/uberproject/MainActivity.kt
+++ app/src/main/java/com/csamanez/uberproject/MainActivity.kt
@@ -2,10 +2,17 @@ package com.csamanez.uberproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Volver transparente el status bar
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
     }
 }
